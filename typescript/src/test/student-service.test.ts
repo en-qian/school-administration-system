0bf008e20c8a4b5d3054250defe145b900121b11
enqian <eqreguse@gmail.com>
2026-03-04 10:13:22 +0800

    Add description

--- typescript/src/test/student-service.test.ts
+++ typescript/src/test/student-service.test.ts
@@ -4,6 +4,7 @@ import { getStudentsByTeacherId } from '../services/students-service';
 describe('get students by teacher id', () => {
   it('should return an object', async () => {
     await expect(
+      // Replace the id and color based on your db data
       getStudentsByTeacherId('74VZn8lwkLemT35QJXSzc00cVlNrsMJCEcMcUlZh', {
         classCode: 'P1-1',
         limit: 10,
